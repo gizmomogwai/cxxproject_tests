dbc4226f7bc61de6ad4ead925f7185619d0700e0
Christian Köstlin <christian.koestlin@esrlabs.com>
2013-01-10 17:55:50 +0100

    added to_github task
    searches for git repositories with a configured github remote
    
    Change-Id: I6fe65f5e2f494e705ad753132450070fa4279ca8

--- Rakefile.rb
+++ Rakefile.rb
@@ -14,8 +14,8 @@ task :overview do
   sh 'gem install terminal-table'
   require 'terminal-table'
   rows = []
-  projects.each do |p,v|
-    rows << [p, v.include?(:SPEC) ? '*' : ' ', v.include?(:ACCEPT) ? '*' : ' ']
+  projects.each do |p,tests|
+    rows << [p, tests.include?(:SPEC) ? '*' : ' ', tests.include?(:ACCEPT) ? '*' : ' ']
   end
   table = Terminal::Table.new(:title => 'Project Overview', :headings => ['Project', 'Unit-Tests', 'Acceptance-Tests'], :rows => rows, :style => {:width => 80})
   puts table
@@ -23,17 +23,45 @@ end
 
 desc 'test all projects'
 task :test_all => [:overview] do
-  projects.each do |p,v|
-    if v.include?(:SPEC) || v.include?(:ACCEPT)
+  projects.each do |p,tests|
+    if tests.include?(:SPEC) || tests.include?(:ACCEPT)
       puts "-" * 80
       puts "- working on '#{p}' in '#{File.absolute_path(File.join(Dir.pwd, '..', p))}'"
       puts "-" * 80
       cd "../#{p}" do
         sh 'rvm --force gemset empty'
         sh 'gem install rspec bundler builder grit'
-        sh 'rake spec' if v.include?(:SPEC)
-        sh 'rake prepare_accept accept' if v.include?(:ACCEPT)
+        sh 'rake spec' if tests.include?(:SPEC)
+        sh 'rake prepare_accept accept' if tests.include?(:ACCEPT)
       end
+    end
+  end
+end
+
+def projects_with_configured_github
+  projects.keys.delete_if do |p|
+    res = true
+    begin
+      cd("../#{p}", :verbose => false) do
+        s = `git remote`
+        res = s.index('github') == nil
+      end
+    rescue => e
+      res = true
+    end
+    res
+  end
+end
+
+desc "upload #{projects_with_configured_github.join(', ')} to github default:master"
+task :to_github do
+  projects_with_configured_github.each do |p|
+    begin
+      cd "../#{p}" do
+        sh 'git push github default:master'
+      end
+    rescue
+      puts "no github repository defined for #{p}"
     end
   end
 end
