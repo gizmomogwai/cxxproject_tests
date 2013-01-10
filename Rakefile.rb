67910970cab47f3167990564db6ea100ae78209f
Christian Köstlin <christian.koestlin@esrlabs.com>
2013-01-10 16:26:16 +0100

    added project list with spec and or accept stages
    Change-Id: Id82a85f5f11c70876565550930bff83f2f6ec883

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,14 +1,18 @@
 def projects
-  ['cxxproject', 'cxx', 'cxxproject_tomake']
+  s = [:SPEC]
+  a = [:ACCEPT]
+  sa = s + a
+  {'cxxproject' => sa, 'cxx' => sa, 'cxxproject_tomake' => sa}
 end
 
 desc 'test all projects'
 task :test_all do
-  projects.each do |p|
+  projects.each do |p,v|
     cd "../#{p}" do
       sh 'rvm --force gemset empty'
-      sh 'gem install rspec'
-      sh 'rake prepare_accept accept'
+      sh 'gem install rspec bundler builder grit'
+      sh 'rake spec' if v.include?(:SPEC)
+      sh 'rake prepare_accept accept' if v.include?(:ACCEPT)
     end
   end
 end
